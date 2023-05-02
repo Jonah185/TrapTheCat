1a036349310207c2976afcae0c8e1443129d4e1a
Jonah Rubenstein <jdawg35r@gmail.com>
2023-05-01 21:57:03 -0400

    Added Comments

--- CatGame.java
+++ CatGame.java
@@ -14,10 +14,18 @@ public class CatGame{
 	EdgeWeightedGraph G;
 	DijkstraUndirectedSP SP;
   /*
-  A CatGame is contructed as an n by n grid of vertices that constitute the board and an additional vertex that represents a successful escape.
-  The rows are offset to make a hexogonal like structure and the edges between them are constructed as follows:
-  All vertices not on the edge are connected to the nodes on their left and right as well as in the row above and below them.
-  Vertices not on the edge are also connected to two additional vertices whose integer representaion differs if they are in an odd or even row.
+  * A CatGame is contructed as an n by n grid of vertices that constitute the board and an additional vertex that represents a successful escape.
+  * The rows are offset to make a hexogonal like structure and the edges between them are constructed as follows:
+  * All vertices not on the exterior are connected to the nodes on their left and right as well as in the row above and below them.
+  * Vertices not on the exterior are also connected to two additional vertices whose integer representaion differs if they are in an odd or even row.
+  * On only even rows we connect each vertex with the vertex above and to the left as well as below and to the left. This assures that all
+  * of the necessary connections are created and are created exactly once.
+
+  * Vertices on the edge are then connected to the "FREEDOM" vertex and only the "FREEDOM" vertex.
+  * All connections from the inside out have already been made and the exterior vertices do not need to be connected to each other because
+  * the shortest path is always directly to the "FREEDOM" vertex
+
+  * Lastly, a random number of  random vertices from [0-n) are marked and a Shortest path with the starting vertex (n/2, n/2) as the source is created
   */
 	public CatGame(int n){
 		this.n = n;
@@ -35,10 +43,6 @@ public class CatGame{
 			if(row % 2 == 0){			
 				G.addEdge(new CatEdge(v, v + n - 1));
 				G.addEdge(new CatEdge(v, v - n - 1));
-			}
-			else{
-				G.addEdge(new CatEdge(v, v + n + 1));
-				G.addEdge(new CatEdge(v, v - n + 1));
 			}
 			marked[v] = false;
 			}
@@ -61,7 +65,17 @@ public class CatGame{
 		}
 		SP = new DijkstraUndirectedSP(G, s);
 	}
-	
+
+	/*
+	 * Takes two ints for row and column as parameters, marks the corresponding vertex and moves the cat along the newly created shortest path. 
+	 * between it's current location and the "FREEDOM" vertex. Marking a vertex has two steps:
+	 * 
+	 * First, change the weight of all of the edges adjacent to the vertex to infinity to effectively remove it from the shortest path tree.
+	 * Second, update the marked array to account for the newly marked edge.
+	 * 
+	 * Then, the cat is moved by constructing the new shortest path tree and setting the cat's position to be the other vertex of the first edge of the shortest
+	 * path between s and the "FREEDOM" vertex.
+	 */
 	public void markTile(int row, int col){
 		int v = getIndex(row, col);
 			for(Edge i : G.adj(v)){
@@ -69,37 +83,51 @@ public class CatGame{
 			c.changeWeight();
 		}
 		SP = new DijkstraUndirectedSP(G, s);
-    if(SP.distTo(FREEDOM) == Double.POSITIVE_INFINITY){
-      CatIsTrapped = true;
-    }
+    	if(SP.distTo(FREEDOM) == Double.POSITIVE_INFINITY){
+     		CatIsTrapped = true;
+    	}
 		else{
-    CatEdge e = (CatEdge) SP.pathTo(FREEDOM).iterator().next();
-		s = e.other(s);
-		marked[v] = true;
-    }
+    		CatEdge e = (CatEdge) SP.pathTo(FREEDOM).iterator().next();
+			s = e.other(s);
+			marked[v] = true;
+    	}
 	}
+
+	/*
+	 * Does the same thing as markTile except it doesnt move the cat.
+	 */
 	private void startMarkTile(int row, int col){
-		marked[getIndex(row, col)] = true;
 		int v = getIndex(row, col);
-			for(Edge i : G.adj(v)){
+		for(Edge i : G.adj(v)){
 			CatEdge c = (CatEdge) i;
 			c.changeWeight();
 		}
+		marked[v] = true;
 	}
+
+	/*
+	 * Takes two ints as parameteres for the row and column and returns the corresponing index of the marked array.
+	 */
 	public boolean marked(int row, int col){
 		return marked[getIndex(row, col)];
 	}
+	/*
+	 * Takes two ints as parameters for the row and column and returns the index representation of the vertex.
+	 */
 	private int getIndex(int row, int col){
 		return n * row + col;
 	}
+	/*
+	 * Takes no parameters and returns an int array containing the row in index 0 and the column in index 1.
+	 */
 	public int[] getCatTile(){
 		return new int[]{s/n,s%n};
 		
 	}	
+	/*
+	 * Returns true if the cat's current position is the "FREEDOM" vertex and false otherwise.
+	 */
 	public boolean catHasEscaped(){
 		return s == FREEDOM;
-	}
-	public boolean catIsTrapped(){
-		return CatIsTrapped;
 	}
 }
