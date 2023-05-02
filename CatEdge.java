dc8928430c876ff1e2a276eb970c89c97cae9a04
Jonah Rubenstein <jdawg35r@gmail.com>
2023-05-01 21:57:40 -0400

    Added Comments

--- CatEdge.java
+++ CatEdge.java
@@ -1,7 +1,7 @@
 import edu.princeton.cs.algs4.Edge;
 /*
-CatEdge extends Sedgewick's Edge.java class and is identical except for it's weight is always one and it has an additional method, the changeWeight() method.
-The changeWeight() method takes no parameters and simply changes the CatEdge object's weight to be infinity
+* CatEdge extends Sedgewick's Edge.java class and is identical except for it's weight is always one and it has an additional method, the changeWeight() method.
+* The changeWeight() method takes no parameters and simply changes the CatEdge object's weight to be infinity
 */
 public class CatEdge extends Edge{
 	private double weight = 1;
